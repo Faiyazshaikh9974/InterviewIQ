8844199dd291aaf1d403640a790d125074d2be8c
faiyazShaikh <98077471+Faiyazshaikh9974@users.noreply.github.com>
2026-06-23 19:40:55 +0530

    Add Redux for state management and implement user slice

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
@@ -8,7 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 );
