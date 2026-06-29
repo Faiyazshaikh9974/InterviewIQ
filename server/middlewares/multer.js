3ca294f99f9a472fd51d5b7df697fceaa1832e76
faiyazShaikh <98077471+Faiyazshaikh9974@users.noreply.github.com>
2026-06-29 12:22:04 +0530

    Enhance multer configuration to set a maximum file size limit of 5MB; refactor askAI function for improved error handling and code clarity

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -14,4 +14,4 @@ export const storage = multer.diskStorage(
     }
 );
 
-export const upload = multer({storage: storage});
+export const upload = multer({storage: storage, limits: {fileSize: 5* 1024 * 1024} }) //5MB file size is max
--- server/services/openRouter.service.js
+++ server/services/openRouter.service.js
@@ -1,17 +1,23 @@
-import axios from  "axios"
+import axios from "axios";
 
-
-export const askAI = (messages)=>{
-
-    try {
-        if(!messages || !isArray(messages) || messages.length === 0 ){
-            throw new Error("Message Array is empty");
-        }
-
-
-        
-    } catch (error) {
-        
+export const askAI = async (messages) => {
+  try {
+    if (!messages || !isArray(messages) || messages.length === 0) {
+      throw new Error("Message Array is empty");
     }
 
-}
+    const response = await axios.post(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+
+        body: { model: "openai/gpt-4o-mini", messages: messages },
+      },
+    );
+
+    console.log(response);
+  } catch (error) {}
+};
